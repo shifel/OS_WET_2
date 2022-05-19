acf959effc9d131dbc38c04b116a605eb1a6e5a9
MrDrprofessorGuy <guycohen801@gmail.com>
2022-05-19 14:28:47 +0300

    update

--- kernel/hw2.c
+++ kernel/hw2.c
@@ -1,5 +1,18 @@
 #include <linux/kernel.h>
+#include <linux/sched.h>
+
 asmlinkage long sys_hello(void) {
  printk("Hello, World!\n");
  return 0;
+}
+asmlinkage int sys_set_status(int status){
+    if(status != 0 && status != 1){
+        return -EINVAL;
+    }
+    current->facultie = status;
+    return 0;
+}
+
+asmlinkage int sys_get_status(void){
+    return current->facultie;
 }
